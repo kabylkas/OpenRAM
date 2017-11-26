48e9f27eb7a623756e76742e47f561dedd9d7d1c
Nursultan Kabylkas <kabylkas@gmail.com>
2017-11-26 09:49:50 -0800

    just saving to checkout other brnach

--- compiler/xor_2.py
+++ compiler/xor_2.py
@@ -16,10 +16,6 @@ class xor_2(design.design):
         self.width = xor_2.width
         self.height = xor_2.height
         self.pin_map = xor_2.pin_map
-        for k,v in self.pin_map.items():
-          print(k,v)
-          for e in v:
-            print("--{0}".format(e.height()))
 
     def analytical_delay(self, slew, load=0.0):
       #TODO
